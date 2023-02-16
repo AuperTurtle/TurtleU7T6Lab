7c83f41b2c3bf9a787bd846d3eed9231a7426946
toxicpuppy123@gmail.com <toxicpuppy123@gmail.com>
2023-02-15 23:55:14 -0500

    ,

--- src/Sort.java
+++ src/Sort.java
@@ -1,2 +1,71 @@
-package PACKAGE_NAME;public class Sort {
+import java.util.ArrayList;
+
+public class Sort {
+
+    // PART A.  Which Sort is Which?
+    public static void selectionSort(int[] elements) {
+        int counter = 0;
+        for (int j = 0; j < elements.length - 1; j++) {
+            int minIndex = j;
+            for (int k = j + 1; k < elements.length; k++) {
+                counter++;
+                if (elements[k] < elements[minIndex]) {
+                    minIndex = k;
+                }
+            }
+            int temp = elements[j];
+            elements[j] = elements[minIndex];
+            elements[minIndex] = temp;
+        }
+        System.out.println("Iterations: " + counter);
+    }
+
+    public static void insertionSort(int[] elements) {
+        int counter = 0;
+        for (int j = 1; j < elements.length; j++) {
+            int temp = elements[j];
+            int possibleIndex = j;
+            while (possibleIndex > 0 && temp < elements[possibleIndex - 1]) {
+                counter++;
+                elements[possibleIndex] = elements[possibleIndex - 1];
+                possibleIndex--;
+            }
+            elements[possibleIndex] = temp;
+        }
+        System.out.println("Iterations: " + counter);
+    }
+
+    // PART B.  Sorting a 1000-word list!
+    public static void selectionSortWordList(ArrayList<String> words) {
+        int counter = 0;
+        for (int j = 0; j < words.size() - 1; j++) {
+            int minIndex = j;
+            for (int k = j + 1; k < words.size(); k++) {
+                counter++;
+                if (words.get(k).compareTo(words.get(minIndex)) < 0) {
+                    minIndex = k;
+                }
+            }
+            String temp = words.get(j);
+            words.set(j, words.get(minIndex));
+            words.set(minIndex, temp);
+        }
+        System.out.println("Iterations: " + counter);
+    }
+
+    public static void insertionSortWordList(ArrayList<String> words) {
+        int counter = 0;
+        for (int j = 1; j < words.size(); j++) {
+            String temp = words.get(j);
+            int possibleIndex = j;
+            while (possibleIndex > 0 && temp.compareTo(words.get(possibleIndex - 1)) < 0) {
+                counter++;
+                words.set(possibleIndex, words.get(possibleIndex - 1));
+                possibleIndex--;
+            }
+            words.set(possibleIndex, temp);
+        }
+        System.out.println("Iterations: " + counter);
+    }
+
 }
--- src/SortTester.java
+++ src/SortTester.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class SortTester {
+import java.util.Arrays;
+
+public class SortTester {
+    public static void main(String[] args) {
+        System.out.println("--- SELECTION SORT --- ");
+        int[] arr1 = {-1, 1, 2};
+        // note the selectionSort method modifies the original array (rather than returning a new array that is sorted)
+        Sort.selectionSort(arr1);
+        System.out.println(Arrays.toString(arr1));
+
+        System.out.println("\n--- INSERTION SORT --- ");
+        int[] arr2 = {-1, 1, 2};
+        // note the insertionSort method modifies the original array (rather than returning a new array that is sorted)
+        Sort.insertionSort(arr2);
+        System.out.println(Arrays.toString(arr2));
+    }
 }
